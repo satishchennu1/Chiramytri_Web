1fc7acc2dda950e6699ae96ed89d84bc3d6bcd71
schennu <satishchennu@gmail.com>
2021-12-25 18:15:43 -0500

    Add files via upload

---
+++ animate.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|Gautham
+vti_modifiedby:SR|Gautham
+vti_timecreated:TR|09 Apr 2016 11:21:36 -0000
+vti_timelastmodified:TR|09 Apr 2016 11:21:36 -0000
+vti_cacheddtm:TX|09 Apr 2016 11:21:36 -0000
+vti_filesize:IR|14261
+vti_extenderversion:SR|4.0.2.8912
+vti_backlinkinfo:VX|
